449261a4167dfb7668a8c0adcee899fca2477fac
aslongassun <huaquocvinh@gmail.com>
2018-05-22 10:52:05 +0700

    commit

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 // db.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://huaquocvinh:conduongmua@ds229380.mlab.com:29380/leaderboard');
+//mongoose.connect('mongodb://huaquocvinh:conduongmua@ds229380.mlab.com:29380/leaderboard');
+mongoose.connect('mongodb://localhost:27017/leaderboard');
